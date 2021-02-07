04fcc085d4187eb5ac992cda53a9d2cef6171bc5
Robzlegz <robzlegz@gmail.com>
2021-02-07 20:26:33 +0200

    adding a box in the gui

--- App.py
+++ App.py
@@ -4,7 +4,11 @@ import os
 
 root = tk.Tk()  # holds the whole app
 
-canvas = tk.Canvas(root, height=700, width=700, bg="#263d42")
-canvas.pack()
+canvas = tk.Canvas(root, height=700, width=700,
+                   bg="#263d42")  # styles the little window
+canvas.pack()  # makes canvas attached to root
+
+frame = tk.Frame(root, bg="#fff")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
 root.mainloop()  # opens a little window
