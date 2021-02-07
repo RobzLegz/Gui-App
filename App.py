122f40d5d354d1ba03c7a329dcff044ad7768e88
Robzlegz <robzlegz@gmail.com>
2021-02-07 20:40:02 +0200

    small changes

--- App.py
+++ App.py
@@ -7,13 +7,17 @@ apps = []
 
 
 def addApp():
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+
     filename = filedialog.askopenfilename(initialdir="/", title="Select File",
                                           filetypes=(("executables", "*exe"), ("all files", "*.*")))  # shows all .exe files
     apps.append(filename)
     for app in apps:
 
         # adds a label with a location of your app
-        label = tk.Label(frame, text=app, bg="#444")
+        label = tk.Label(frame, text=app, bg="grey")
         label.pack()
 
 
