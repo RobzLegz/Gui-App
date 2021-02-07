8cd9226c56b6596c3f9536a6c7b227235e4b2119
Robzlegz <robzlegz@gmail.com>
2021-02-07 20:33:24 +0200

    adding open file option

--- App.py
+++ App.py
@@ -4,6 +4,12 @@ import os
 
 root = tk.Tk()  # holds the whole app
 
+
+def addApp():
+    filename = filedialog.askopenfilename(initialdir="/", title="Select File",
+                                          filetypes=(("executables", "*exe"), ("all files", "*.*")))
+
+
 canvas = tk.Canvas(root, height=700, width=700,
                    bg="#263d42")  # styles the little window
 canvas.pack()  # makes canvas attached to root
@@ -11,8 +17,11 @@ canvas.pack()  # makes canvas attached to root
 frame = tk.Frame(root, bg="#fff")  # adds a little box in the popup
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1,
             rely=0.1)  # sizes of the box
-openFile = tk.Button(root, text="Open Files", padx=10,
-                     pady=5, fg="#fff", bg="#263d42")
+openFile = tk.Button(root, text="Open File", padx=10,
+                     pady=5, fg="#fff", bg="#263d42", command=addApp)
 openFile.pack()
+runApps = tk.Button(root, text="Run Apps", padx=10,
+                    pady=5, fg="#fff", bg="#263d42")
+runApps.pack()
 
 root.mainloop()  # opens a little window
