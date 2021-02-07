d13ce2a57aacdcdefcaf87c8dcda9643e01cfde3
Robzlegz <robzlegz@gmail.com>
2021-02-07 20:37:47 +0200

    saving open file names

--- App.py
+++ App.py
@@ -1,13 +1,20 @@
 import tkinter as tk
-from tkinter import filedialog, Text
+from tkinter import Label, filedialog, Text
 import os
 
 root = tk.Tk()  # holds the whole app
+apps = []
 
 
 def addApp():
     filename = filedialog.askopenfilename(initialdir="/", title="Select File",
-                                          filetypes=(("executables", "*exe"), ("all files", "*.*")))
+                                          filetypes=(("executables", "*exe"), ("all files", "*.*")))  # shows all .exe files
+    apps.append(filename)
+    for app in apps:
+
+        # adds a label with a location of your app
+        label = tk.Label(frame, text=app, bg="#444")
+        label.pack()
 
 
 canvas = tk.Canvas(root, height=700, width=700,
