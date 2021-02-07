0cfb05286b5062e3ad021e40bf023088e993225e
Robzlegz <robzlegz@gmail.com>
2021-02-07 20:28:36 +0200

    adding a button

--- App.py
+++ App.py
@@ -8,7 +8,11 @@ canvas = tk.Canvas(root, height=700, width=700,
                    bg="#263d42")  # styles the little window
 canvas.pack()  # makes canvas attached to root
 
-frame = tk.Frame(root, bg="#fff")
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+frame = tk.Frame(root, bg="#fff")  # adds a little box in the popup
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1,
+            rely=0.1)  # sizes of the box
+openFile = tk.Button(root, text="Open Files", padx=10,
+                     pady=5, fg="#fff", bg="#263d42")
+openFile.pack()
 
 root.mainloop()  # opens a little window
