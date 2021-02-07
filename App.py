9c924023a39425a019274ad947ec5c9c31342a05
Robzlegz <robzlegz@gmail.com>
2021-02-07 20:51:18 +0200

    printing app when gui closed

--- App.py
+++ App.py
@@ -5,6 +5,12 @@ import os
 root = tk.Tk()  # holds the whole app
 apps = []
 
+if os.path.isfile("save.txt"):
+    with open("save.txt", "r") as f:
+        tempApps = f.read()
+        tempApps = tempApps.split(",")
+        print(tempApps)
+
 
 def addApp():
 
